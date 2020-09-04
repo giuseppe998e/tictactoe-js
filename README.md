b40989e6ad45ed9c1fd368934a9d38212ecfbe18
Giuseppe Eletto <peppe.eletto@gmail.com>
2020-09-04 19:30:45 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Another TicTacToe Script (that uses MiniMax algorithm)
+  > **Tic-tac-toe** (American English), **noughts and crosses** (Commonwealth English), or **Xs and Os** is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner.
+
+This script uses the MiniMax algorithm to calculate the moves of the computer, it is practically impossible to beat it.
+
+Minified versions of the codes are available in the "example" folder.<br>
+
+P.S. :: *At the moment, the only implemented "difficulty" is the "impossible" one.*
---
+++ tictactoe.js
@@ -0,0 +1,310 @@
+/**
+ * Copyright (C) 2020 Giuseppe Eletto
+ * 
+ * "TicTacToe.js" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * "TicTacToe.js" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with "TicTacToe.js". If not, see <http://www.gnu.org/licenses/>.
+ */
+
+"use strict";
+
+const TicTacToe = function(boardId) {
+  // ---------------------------------------------
+  // Private Constants
+  // ---------------------------------------------
+  const WIN_CONDITIONS = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8]]
+
+  const PLAYER = {
+    Human: -1,
+    Blank: 0,
+    Computer: 1
+  }
+
+  const UI_BOARD = (() => { // Called one time only
+    if (!(typeof boardId === 'string' && boardId[0] === '#')) {
+      throw 'Board not found!'
+    }
+
+    return document.querySelectorAll(`${boardId} .ttt-box`)
+  })()
+
+  // ---------------------------------------------
+  // Private Variables
+  // ---------------------------------------------
+  let boardState = Array(9).fill(PLAYER.Blank)
+
+  let humanWins = 0
+  let computerWins = 0
+
+  let difficulty = 2 // 0 = Easy, 1 = Normal, 2 = Impossible
+  let playerTurn = PLAYER.Human
+
+  let winListener = null
+  let drawListener = null
+  let pointsListener = null
+
+  // ---------------------------------------------
+  // Private Methods
+  // ---------------------------------------------
+  const matchHandler = () => {
+    const isVictory = isWinMove()
+    const isDraw = isDrawMove()
+
+    if (isVictory || isDraw) {
+      if (isVictory) {
+        if (isHumanTurn()) {
+          console.log('Player "Human" WINS!')
+          playerTurn = PLAYER.Human
+          humanWins += 1
+        } else {
+          console.log('Player "Computer" WINS!')
+          playerTurn = PLAYER.Computer
+          computerWins += 1
+        }
+    
+        if (typeof winListener == 'function') {
+          winListener(playerTurn == PLAYER.Human ? 'Human' : 'Computer')
+        }
+    
+        if (typeof pointsListener == 'function') {
+          pointsListener(humanWins, computerWins)
+        }
+      }
+
+      else if (isDraw) {
+        console.log('DRAW!')
+
+        if (typeof drawListener == 'function') {
+          drawListener()
+        }
+      }
+      
+      setTimeout(() => this.init(), 350)
+      return true
+    }
+
+    return false
+  }
+
+  const computerTurn = () => {
+    if (!isHumanTurn()) {
+      let move = -1
+      
+      switch (difficulty) {
+        case 0:
+          move = easyMode()
+          break
+        case 1:
+          move = fairMode()
+          break
+        case 2:
+          move = impossibleMode()
+          break
+      }
+  
+      if (markBox(move)) {
+        if (!matchHandler()) {
+          playerTurn = PLAYER.Human
+        }
+      }
+    }
+  }
+
+  const easyMode = () => {
+    throw 'To be implemented!' // TODO EasyMode
+  }
+
+  const fairMode = () => {
+    throw 'To be implemented!' // TODO FairMode
+  }
+
+  const impossibleMode = () => {
+    let score = -2
+    let move = -1
+
+    for (let x = 0; x < boardState.length; x++) {
+      if (isBlankBox(x)) {
+        const clonedBoard = boardState.slice()
+        clonedBoard[x] = PLAYER.Computer
+
+        const nextMoveScore = -calcMiniMax(clonedBoard, PLAYER.Human)
+  
+        if (nextMoveScore > score) {
+          score = nextMoveScore
+          move = x
+        }
+      }
+    }
+
+    return move
+  }
+
+  const calcMiniMax = (board, player) => {
+    const winner = getWinner(board)
+    if (winner !== false) {
+      return winner * player
+    }
+
+    let score = -2
+    let move = -1
+
+    for (let x = 0; x < board.length; x++) {
+      if (!isBlankBox(x, board)) {
+        continue
+      }
+      
+      const clonedBoard = board.slice()
+      clonedBoard[x] = player
+
+      const nextMoveScore = -calcMiniMax(clonedBoard, getOpponentFor(player))
+      if (nextMoveScore > score) {
+        score = nextMoveScore
+        move = x
+      }
+    }
+
+    if (move == -1) {
+      return 0 // Can't move ==> draw!
+    }
+
+    return score
+  }
+
+  const markBox = index => {
+    if (isNaN(index)) {
+      throw `"${index}" is not a number!`
+    }
+
+    if (isBlankBox(index)){
+      if (isHumanTurn()) {
+        boardState[index] = PLAYER.Human
+        UI_BOARD[index].classList.add('human')
+        return true
+      } else {
+        boardState[index] = PLAYER.Computer
+        UI_BOARD[index].classList.add('computer')
+        return true
+      }
+    }
+
+    return false
+  }
+
+  const isDrawMove = (board = boardState) => {
+    for (let x = 0; x < board.length; x++) {
+      if (isBlankBox(x, board)) {
+        return false
+      }
+    }
+    return true
+  }
+
+  const isWinMove = (player = playerTurn, board = boardState) => {
+    return getWinner(board) == player
+  }
+
+  const getWinner = (board = boardState) => {
+    for (let x = 0; x < WIN_CONDITIONS.length; x++) {
+      const condition = WIN_CONDITIONS[x]
+      const player = board[condition[0]]
+
+      if (player == PLAYER.Blank) {
+        continue
+      }
+
+      if (board[condition[1]] == player && board[condition[2]] == player) {
+        return player
+      }
+    }
+
+    return false
+  }
+
+  const isBlankBox = (index, board = boardState) => {
+    return board[index] == PLAYER.Blank
+  }
+
+  const isHumanTurn = () => {
+    return playerTurn == PLAYER.Human
+  }
+
+  const getOpponentFor = (player = playerTurn) => {
+    if (player !== 1 && player !== -1) {
+      throw `${player} is not a valid input!`
+    }
+
+    return player * -1
+  }
+
+  // ---------------------------------------------
+  // Public Methods
+  // ---------------------------------------------
+  this.init = () => {
+    // Reset Board state
+    boardState = Array(9).fill(PLAYER.Blank)
+
+    // Reset UI Board
+    UI_BOARD.forEach((item) => {
+      item.classList.remove('human', 'computer')
+    })
+
+    if (!isHumanTurn()) {
+      computerTurn()
+    }
+  }
+
+  this.setDifficulty = d => {
+    if (isNaN(d)) {
+      console.error(`"${d}" is not a number!`)
+    }
+
+    if (!(d >= 0 && d <= 2)) {
+      console.error(`"${d}" is not between 0 and 2 (included)!`)
+    }
+
+    difficulty = d
+    this.init()
+  }
+
+  this.setWinListener = fn => {
+    winListener = fn
+    return this
+  }
+
+  this.setDrawListener = fn => {
+    drawListener = fn
+    return this
+  }
+
+  this.setPointsListener = fn => {
+    pointsListener = fn
+    return this
+  }
+
+  // ---------------------------------------------
+  // Main Function
+  // ---------------------------------------------
+  UI_BOARD.forEach((item, index) => { // Bind boxes
+    item.addEventListener('click', () => {
+      if (isHumanTurn()) {
+        if (markBox(index)) {
+          if (!matchHandler()) {
+            playerTurn = PLAYER.Computer
+            computerTurn()
+          }
+        }
+      }
+    })
+  })
+
+  this.init() // Start Match
+}
